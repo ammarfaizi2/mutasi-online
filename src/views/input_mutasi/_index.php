ca2a2632e50837e210641f56674ba15813f20a3e
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-20 11:13:05 +0700

    mk view input mutasi

---
+++ src/views/input_mutasi/_index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Input Mutasi</title>
+<style>
+.fcg{
+	margin-top:3%;
+}
+table {
+	border-collapse: collapse;
+}
+#thd {
+	padding-left:3%;
+	padding-right:3%;
+}
+.ia{
+	width:30%;
+}
+.ii{
+	
+}
+</style>
+</head>
+<body>
+<center>
+<div>
+<a href="?"><button>Kembali</button></a>
+</div>
+<div class="fcg">
+<form method="post" action="">
+<table border="1">
+<thead>
+<tr><th colspan="2" id="thd"><h3>Input Mutasi</h3></th></tr>
+</thead>
+<tbody>
+<tr><td class="ia">Nopol</td><td><input type="text" name="nopol"></td></tr>
+<tr><td class="ia">Nama Pemilik</td><td><input type="text" name="nama_pemilik"></td></tr>
+<tr><td class="ia">No Rangka</td><td><input type="text" name="no_rangka"></td></tr>
+<tr><td class="ia">No Mesin</td><td><input type="text" name="no_mesin"></td></tr>
+<tr><td class="ia">No BPKB</td><td><input type="text" name="no_bpkb"></td></tr>
+<tr><td class="ia">No STNK</td><td><input type="text" name="no_stnk"></td></tr>
+<tr><td class="ia">No HP</td><td><input type="text" name="no_hp"></td></tr>
+</tbody>
+<tbody>
+<tr><td>File STNK</td><td><input type="file" name="stnk"></td></tr>
+<tr><td>File STNK</td><td><input type="file" name="stnk"></td></tr>
+<tr><td>File STNK</td><td><input type="file" name="stnk"></td></tr>
+<tr><td>File STNK</td><td><input type="file" name="stnk"></td></tr>
