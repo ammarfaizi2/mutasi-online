6ebd604a5c036116cb7b5c869085f949d60fb552
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-24 00:08:26 +0700

    Update config

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
-/*define("DBHOST", "localhost");
+define("DBHOST", "localhost");
 define("DBUSER", "tegaljat_mutasi");
 define("DBPASS", "triosemut123");
-define("DBNAME", "tegaljat_mutasi");*/
+define("DBNAME", "tegaljat_mutasi");
 
-define("DBHOST", "localhost");
+/*define("DBHOST", "localhost");
 define("DBUSER", "debian-sys-maint");
 define("DBPASS", "");
 define("DBNAME", "mutasi");
-
+*/
 /*define("DBHOST", "localhost");
 define("DBUSER", "root");
 define("DBPASS", "");
