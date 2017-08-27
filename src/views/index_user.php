a2d32973b00ef22a9ccbbd0b4aaafff29cce2f68
Ammar Faizi <ammarfaizi2@gmail.com>
2017-08-27 14:02:52 +0700

    Update index view

--- src/views/index_user.php
+++ src/views/index_user.php
@@ -13,7 +13,7 @@ $nama_polres = $st[0];
 ?>
 <!DOCTYPE html>
 <html>
-<head>
+<head> 
 <title>Mutasi Online</title>
 <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 <link rel="stylesheet" href="assets/css/font-awesome.min.css">
@@ -22,41 +22,70 @@ $nama_polres = $st[0];
 <style>
 body {
 	background-color: #FAFCC2;
+	background-image: url(assets/bg.jpg);
+  	background-position: center center;  
+  	background-repeat: no-repeat;
+  	background-attachment: fixed;
+  	background-size: cover;
+  	background-color: #464646;
 }
 .cg {
 	margin-top: 3%;
 }
-.btg {
-	margin-top: 1%;
-}
-.btn {
-	width: 20%;
-	color:#fff;
-	font-family: Helvetica;
-	font-weight: bold;
-}
 .wcf {
 	margin-top: 3%;
+}
+.cgf{
+	background-color: #fff;
+	width: 35%;
+	margin-top: 7%;
+	padding-top: 1%;
+	padding-bottom: 2%;
+	margin-bottom: 10%;
+	border: 3px solid #000;
+}
+.btgg{
+	font-family: Helvetica, Arial;
+	padding: 2% 14% 2% 14%;
+	color:#fff;
+	background-color:#39C21B;
+	border: 3px solid #000;
+	border-radius: 3%;
+	font-size: 145%;
+	font-weight: bolder;
+}
+.btgg:hover{
+	background-color:#41A326;
+}
+.bfg{
+	font-family: Helvetica, Arial;
+	padding: 2% 14% 2% 14%;
+	color: #fff;
+	font-size: 125%;
+	background-color: #EC971F;
+	border-radius: 3%;
+	font-size: 145%;
+	font-weight: bolder;
+}
+.bfg:hover{
+	background-color: #D18A2C;
 }
 </style>
 </head>
 <body>
 <center>
-	<div class="wcf">
-		<h1 style="font-family:Helvetica;font-weight:bold;">Mutasi Online Polres <?php print $nama_polres; ?></h1>
-	</div>
-	<div class="cg jumbotron">
-		<div class="btg">
-		 	<a href="?pg=input_mutasi"><button class="btn btn-lg btn-success">Input Mutasi Keluar</button></a>
+	<div class="cgf">
+		<div>
+			<img src="assets/logo.jpg" width="170" height="150">
 		</div>
-		<div class="btg">
-			<a href="?pg=permohonan_keluar"><button class="btn btn-lg btn-success">Data Mutasi Keluar</button></a>
+		<div class="wcf">
+			<h2 style="font-family:Helvetica;font-weight:bold;">Mutasi Online Polres <?php print $nama_polres; ?></h2>
 		</div>
-		<div class="btg">
-			<a href="?pg=permohonan_masuk"><button class="btn btn-lg btn-success">Data Mutasi Masuk</button></a>
-		</div>
-		<div class="btg">
-			<a href="?pg=logout"><button class="btn btn-lg btn-warning">Logout</button></a>
+		<div class="cg">
+			<p><a href="?pg=input_mutasi"><button class="btgg">Input Mutasi Keluar</button></a></p>
+			<p><a href="?pg=permohonan_keluar"><button class="btgg">Data Mutasi Keluar</button></a></p>
+			<p><a href="?pg=permohonan_masuk"><button class="btgg">Data Mutasi Masuk</button></a></p>
+			<p><a href="?pg=logout"><button class="bfg">Logout</button></a></p>
 		</div>
 	</div>
 </center>
